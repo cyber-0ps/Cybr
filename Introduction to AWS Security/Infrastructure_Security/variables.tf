3a9dc21be6486e42ab78afddb100213268f08166
Kuljot Biring <ksbiring1@gmail.com>
2025-06-09 19:30:53 -0400

    add CIDR block for VPC

--- Introduction to AWS Security/Infrastructure_Security/variables.tf
+++ Introduction to AWS Security/Infrastructure_Security/variables.tf
@@ -4,3 +4,10 @@ variable "aws_region" {
   type = string
   default = "us-east-1"
 }
+
+# VPC CIDR block
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type = string
+  default = "10.0.0.0/16"
+}
