0a607d93947a483cb06849cdb7a96524bee341e7
Kuljot Biring <ksbiring1@gmail.com>
2025-06-13 18:11:30 -0400

    associate public route table

--- Introduction to AWS Security/Infrastructure_Security/main.tf
+++ Introduction to AWS Security/Infrastructure_Security/main.tf
@@ -44,3 +44,10 @@ resource "aws_route_table" "public" {
     Name = "cybr-public-rt"
   }
 }
+
+# associate public subnets with public route table
+resource "aws_route_table_association" "public" {
+  count = 2
+  subnet_id = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
