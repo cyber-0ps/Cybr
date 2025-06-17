f5ade3b14fa7a2921a95d1805b87cfb24b9ceb1c
Kuljot Biring <ksbiring1@gmail.com>
2025-06-17 09:56:53 -0400

    add s3 vpc endpoint id

--- Introduction to AWS Security/Infrastructure_Security/outputs.tf
+++ Introduction to AWS Security/Infrastructure_Security/outputs.tf
@@ -12,3 +12,8 @@ output "private_subnet_ids" {
   description = "IDs of the private subnets"
   value = [for subnet in aws_subnet.private : subnet.id]
 }
+
+output "s3_vpc_endpoint_id" {
+  description = "ID of the S3 Gateway VPC Endpoint"
+  value = aws_vpc_endpoint.s3.id
+}
