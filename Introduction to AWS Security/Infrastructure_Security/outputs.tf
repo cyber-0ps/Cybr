25851dad84f3e71a4c2ec08166daf648d30c2c5b
Kuljot Biring <ksbiring1@gmail.com>
2025-06-17 09:55:01 -0400

    add output for private subnet id

--- Introduction to AWS Security/Infrastructure_Security/outputs.tf
+++ Introduction to AWS Security/Infrastructure_Security/outputs.tf
@@ -7,3 +7,8 @@ output "public_subnet_ids" {
   description = "IDs of the public subnets"
   value = [for subnet in aws_aws_subnet.public : subnet.id]
 }
+
+output "private_subnet_ids" {
+  description = "IDs of the private subnets"
+  value = [for subnet in aws_subnet.private : subnet.id]
+}
