9b58d48df5fb429a0632168e6e984e04fd5fde76
Kuljot Biring <ksbiring1@gmail.com>
2025-06-19 16:05:04 -0400

    change route map to route block

--- Introduction to AWS Security/Infrastructure_Security/main.tf
+++ Introduction to AWS Security/Infrastructure_Security/main.tf
@@ -39,7 +39,7 @@ resource "aws_subnet" "public" {
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
-  route = {
+  route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
