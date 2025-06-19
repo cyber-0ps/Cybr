7022f1bf0d9c2241d7a701567fd4f4733d67f906
Kuljot Biring <ksbiring1@gmail.com>
2025-06-19 15:29:04 -0400

    correct output file value

--- Introduction to AWS Security/Infrastructure_Security/outputs.tf
+++ Introduction to AWS Security/Infrastructure_Security/outputs.tf
@@ -5,7 +5,7 @@ output "vpc_id" {
 
 output "public_subnet_ids" {
   description = "IDs of the public subnets"
-  value       = [for subnet in aws_aws_subnet.public : subnet.id]
+  value       = [for subnet in aws_subnet.public : subnet.id]
 }
 
 output "private_subnet_ids" {
