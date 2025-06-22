c78b55e797cecb142d27a54561380c086db8ad7f
Kuljot Biring <ksbiring1@gmail.com>
2025-06-22 11:20:05 -0400

    add terraform and required providers block

---
+++ Introduction to AWS Security/Infrastructure_Security/SG_NACL_Lab/terraform.tf
@@ -0,0 +1,12 @@
+# Terraform configuration specifying required versions
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = ">= 5.0"
+    }
+  }
+}
+
