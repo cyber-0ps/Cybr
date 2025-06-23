3b7452239ec142c59e60fc57025ed34041ffd978
Kuljot Biring <ksbiring1@gmail.com>
2025-06-23 10:01:04 -0400

    add CIDR block for variable

--- Introduction to AWS Security/Infrastructure_Security/SG_NACL_Lab/variables.tf
+++ Introduction to AWS Security/Infrastructure_Security/SG_NACL_Lab/variables.tf
@@ -4,3 +4,9 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type = string
+  default = "10.0.0.0/16"
+}
