45ee1f51bde85ace934b923ebb5bacf5d8abb9a9
Kuljot Biring <ksbiring1@gmail.com>
2025-06-23 10:01:26 -0400

    format CIDR block

--- Introduction to AWS Security/Infrastructure_Security/SG_NACL_Lab/variables.tf
+++ Introduction to AWS Security/Infrastructure_Security/SG_NACL_Lab/variables.tf
@@ -7,6 +7,6 @@ variable "aws_region" {
 
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
-  type = string
-  default = "10.0.0.0/16"
+  type        = string
+  default     = "10.0.0.0/16"
 }
