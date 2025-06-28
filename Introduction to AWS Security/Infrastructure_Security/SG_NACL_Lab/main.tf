4af3757acc6ac10461485e9280df2d12592e8279
Kuljot Biring <ksbiring1@gmail.com>
2025-06-28 11:45:27 -0400

    format code

--- Introduction to AWS Security/Infrastructure_Security/SG_NACL_Lab/main.tf
+++ Introduction to AWS Security/Infrastructure_Security/SG_NACL_Lab/main.tf
@@ -1,7 +1,7 @@
 # VPC
 resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
-  enable_dns_support = true
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
@@ -11,30 +11,30 @@ resource "aws_vpc" "main" {
 
 # Security Groups: Web Servers
 resource "aws_security_group" "web_servers" {
-  name = var.web_sg_name
+  name        = var.web_sg_name
   description = "Allow HTT and HTTP from anywhere"
-  vpc_id = aws_vpc.main
+  vpc_id      = aws_vpc.main
 
   # allow port 80 ingress
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress = {
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   # redundant but useful for update later (since it allows all outbound protocols - however you may want to restrict this)
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
--- Introduction to AWS Security/Infrastructure_Security/SG_NACL_Lab/variables.tf
+++ Introduction to AWS Security/Infrastructure_Security/SG_NACL_Lab/variables.tf
@@ -12,28 +12,28 @@ variable "vpc_cidr" {
 }
 
 variable "it_admin_ips" {
-    description = "List of CIDR blocks for IT admin access"
-    type = list(string)
-    default = [ "172.16.0.0/16", "192.168.0.0/16" ]
+  description = "List of CIDR blocks for IT admin access"
+  type        = list(string)
+  default     = ["172.16.0.0/16", "192.168.0.0/16"]
 }
 
 variable "web_sg_name" {
-  type        = string
+  type    = string
   default = "web-server"
 }
 
 variable "app_sg_name" {
-  type        = string
+  type    = string
   default = "app-servers"
 }
 
 variable "it_sg_name" {
-  type        = string
+  type    = string
   default = "it-administrator"
 }
 
 variable "db_sg_name" {
-  
+
   default = "database"
 }
 
