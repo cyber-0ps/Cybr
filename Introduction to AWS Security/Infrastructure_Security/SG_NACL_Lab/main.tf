5112c4c3636012aa42fd606ade7130e4f4562431
Kuljot Biring <ksbiring1@gmail.com>
2025-07-03 14:49:03 -0700

    format code

--- Introduction to AWS Security/Infrastructure_Security/SG_NACL_Lab/main.tf
+++ Introduction to AWS Security/Infrastructure_Security/SG_NACL_Lab/main.tf
@@ -117,22 +117,22 @@ resource "aws_network_acl" "public" {
 
 resource "aws_network_acl_rule" "public_ingress" {
   network_acl_id = aws_network_acl.public.id
-  rule_number = 100
-  egress = false
-  protocol = "-1"
-  rule_action = "allow"
-  cidr_block = "0.0.0.0/0"
-  from_port = 0
-  to_port = 0
+  rule_number    = 100
+  egress         = false
+  protocol       = "-1"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 0
+  to_port        = 0
 }
 
 resource "aws_network_acl_rule" "public_egress" {
   network_acl_id = aws_network_acl.public.id
-  rule_number = 100
-  egress = false
-  protocol = "-1"
-  rule_action = "allow"
-  cidr_block = "0.0.0.0/0"
-  from_port = 0
-  to_port = 0
+  rule_number    = 100
+  egress         = false
+  protocol       = "-1"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 0
+  to_port        = 0
 }
