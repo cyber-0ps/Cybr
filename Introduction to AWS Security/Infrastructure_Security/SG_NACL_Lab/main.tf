5ebbbb2a251056e255c0bcbba3a14fb5a54ae874
Kuljot Biring <ksbiring1@gmail.com>
2025-07-02 17:02:02 -0700

    add egress for sg database

--- Introduction to AWS Security/Infrastructure_Security/SG_NACL_Lab/main.tf
+++ Introduction to AWS Security/Infrastructure_Security/SG_NACL_Lab/main.tf
@@ -97,4 +97,11 @@ resource "aws_security_group" "database" {
     protocol = "tcp"
     security_groups = [aws_security_group.app_servers]
   }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
