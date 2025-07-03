51bc6376a9640eb915e7557260e744ab8c4ab497
Kuljot Biring <ksbiring1@gmail.com>
2025-07-02 17:02:37 -0700

    formatted code

--- Introduction to AWS Security/Infrastructure_Security/SG_NACL_Lab/main.tf
+++ Introduction to AWS Security/Infrastructure_Security/SG_NACL_Lab/main.tf
@@ -45,63 +45,63 @@ resource "aws_security_group" "web_servers" {
 
 # Security Group: App Servers
 resource "aws_security_group" "app_servers" {
-  name = var.app_sg_name
+  name        = var.app_sg_name
   description = "Allow traffic from web servers"
-  vpc_id = aws_vpc.main.id
-  
+  vpc_id      = aws_vpc.main.id
+
   ingress {
-    from_port = 0
-    to_port = 65535
-    protocol = "tcp"
+    from_port       = 0
+    to_port         = 65535
+    protocol        = "tcp"
     security_groups = [aws_security_group.web_servers.id]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 # Security Group: IT Administration
 resource "aws_security_group" "it_admin" {
-  name = var.it_sg_name
+  name        = var.it_sg_name
   description = "Allow SSH and RDP from IT admin IPs"
-  vpc_id = aws_vpc.main.id
+  vpc_id      = aws_vpc.main.id
 
   ingress {
-    from_port = 3389
-    to_port = 3389
-    protocol = "tcp"
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 # Security Group: Database
 resource "aws_security_group" "database" {
-  name = var.db_sg_name
+  name        = var.db_sg_name
   description = "Allow MySQL access from app servers"
-  vpc_id = aws_vpc.main.id
+  vpc_id      = aws_vpc.main.id
 
   ingress {
-    from_port = 3306
-    to_port = 3306
-    protocol = "tcp"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
     security_groups = [aws_security_group.app_servers]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
