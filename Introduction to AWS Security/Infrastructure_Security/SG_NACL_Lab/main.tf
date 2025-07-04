157bbb711964d65e8b7cf7305749b0bc51d79d85
Kuljot Biring <ksbiring1@gmail.com>
2025-07-04 09:54:21 -0700

    add private NACL ingress rule

--- Introduction to AWS Security/Infrastructure_Security/SG_NACL_Lab/main.tf
+++ Introduction to AWS Security/Infrastructure_Security/SG_NACL_Lab/main.tf
@@ -148,3 +148,13 @@ resource "aws_network_acl" "private" {
   }
 }
 
+resource "aws_network_acl_rule" "private_ingress" {
+  network_acl_id = aws_network_acl.private.id
+  rule_number    = 100
+  egress         = false
+  protocol       = "-1"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 0
+  to_port        = 0
+}
