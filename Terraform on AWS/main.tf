141b27a45d46f1c66740d9bc8a9fccb950d18d4b
Kuljot Biring <ksbiring1@gmail.com>
2025-07-13 11:34:41 -0700

    update main.tf

--- Terraform on AWS/main.tf
+++ Terraform on AWS/main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "6.3.0"
-    }
-  }
-}
-
-provider "aws" {
-  # Configuration options
-  region = "us-east-1"
-  profile = "cybr"
-}
-
 resource "aws_s3_bucket" "example" {
   bucket = "kuljot-biring-test"
 
