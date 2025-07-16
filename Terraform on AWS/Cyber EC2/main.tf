6479d2f6a24c95997a8e84fc60846f01de5fcf12
Kuljot Biring <ksbiring1@gmail.com>
2025-07-15 17:29:45 -0700

    create subnet in VPC

--- Terraform on AWS/Cyber EC2/main.tf
+++ Terraform on AWS/Cyber EC2/main.tf
@@ -13,3 +13,12 @@ data "aws_availability_zones" "available" {
 locals {
   filtered_azs = [for az in data.aws_availability_zones.available.names : az if contains(var.desired_azs, az)]
 }
+
+// Subnet within the VPC
+resource "aws_subnet" "main" {
+  vpc_id            = aws_vpc.custom_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = element(local.filtered_azs, 0)
+
+  tags = var.aws_tagging
+}
