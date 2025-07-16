44aebc2aae01bac9c9b495ecc3315528c63cb2d1
Kuljot Biring <ksbiring1@gmail.com>
2025-07-15 17:31:52 -0700

    add security group

--- Terraform on AWS/Cyber EC2/main.tf
+++ Terraform on AWS/Cyber EC2/main.tf
@@ -22,3 +22,30 @@ resource "aws_subnet" "main" {
 
   tags = var.aws_tagging
 }
+
+// Security group
+resource "aws_security_group" "example" {
+  name        = "Example Security Group"
+  description = "Example security group for EC2 instance"
+  vpc_id      = aws_vpc.custom_vpc.id
+
+  tags = var.aws_tagging
+}
+
+// Ingress rule
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.example.id
+
+  cidr_ipv4   = aws_vpc.custom_vpc.cidr_block
+  from_port   = 80
+  to_port     = 80
+  ip_protocol = "tcp"
+}
+
+// Egress rule
+resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
+  security_group_id = aws_security_group.example.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = "-1"
+}
