0e7d421387cfeba7363500ae3e9de81ab75ae618
Kuljot Biring <ksbiring1@gmail.com>
2025-07-16 14:03:54 -0700

    update main.tf with imported bucket

--- Terraform on AWS/LAB import an AWS resource/main.tf
+++ Terraform on AWS/LAB import an AWS resource/main.tf
@@ -1,3 +1,9 @@
+import {
+  to = aws_s3_bucket.bucket_to_import
+  id = "demo-import-bucket"
+}
+
 resource "aws_s3_bucket" "bucket_to_import" {
-  
+    bucket = "demo-import-bucket"
+    force_destroy = true
 }
