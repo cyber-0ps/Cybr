c7f6012aa7b6772e00c0e7621930cf0c354b7bff
Kuljot Biring <ksbiring1@gmail.com>
2025-07-17 19:18:17 -0700

    add the networking module

---
+++ Terraform on AWS/project-ssm/main.tf
@@ -0,0 +1,15 @@
+# Create VPC Network
+module "networking" {
+  source = "./modules/vpc"
+
+  vpc_config = {
+    cidr_block           = "10.0.0.0/16"
+    enable_dns_hostnames = true
+    enable_dns_support   = true
+    availability_zones   = ["us-east-1a", "us-east-1b"]
+
+    tags = {
+      "Name" = "tf_vpc_1"
+    }
+  }
+}
