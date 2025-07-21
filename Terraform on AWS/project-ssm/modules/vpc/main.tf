b28dfcf801f910e05fb576251474093bb2666bba
Kuljot Biring <ksbiring1@gmail.com>
2025-07-21 11:15:00 -0700

    add public route table

--- Terraform on AWS/project-ssm/modules/vpc/main.tf
+++ Terraform on AWS/project-ssm/modules/vpc/main.tf
@@ -34,3 +34,13 @@ resource "aws_subnet" "public" {
     "Name" = "tf_public_subnet_${count.index +1}"
   }  
 }
+
+# Create route table for public subnets
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc.id
+  route = {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw
+  }
+}
+
