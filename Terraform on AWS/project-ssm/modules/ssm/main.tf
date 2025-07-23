1dfd043227330940035815e3f6a225019acea1f7
Kuljot Biring <ksbiring1@gmail.com>
2025-07-22 22:11:04 -0700

    add IAM role

--- Terraform on AWS/project-ssm/modules/ssm/main.tf
+++ Terraform on AWS/project-ssm/modules/ssm/main.tf
@@ -4,3 +4,22 @@ data "aws_region" "current" {}
 # get current account ID
 data "aws_caller_identity" "current" {}
 
+# create IAM role for SSM
+resource "aws_iam_role" "default_host_management" {
+    name = "AWSSystemsManagerDefaultEC2InstanceManagementRole"
+    description = "AWS Systems Manager Default EC2 Instance Management Role"
+    assume_role_policy = jsonencode(
+        {
+      Statement = [
+        {
+          Action = "sts:AssumeRole"
+          Effect = "Allow"
+          Principal = {
+            Service = "ssm.amazonaws.com"
+          }
+        },
+      ]
+      Version = "2012-10-17"
+    }
+    )
+}
