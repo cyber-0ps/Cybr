190af4311ba71931cc4755ad64fadcd2daa04ab4
Kuljot Biring <ksbiring1@gmail.com>
2025-07-22 22:18:52 -0700

    add output for SSM

---
+++ Terraform on AWS/project-ssm/modules/ssm/outputs.tf
@@ -0,0 +1,8 @@
+# output map of ssm related resources
+output "ssm_resources" {
+    value = {
+        iam_arn = aws_iam_role.default_host_management.arn
+        iam_name = aws_iam_role.default_host_management.name
+    }
+  description = "SSM resources properties"
+}
