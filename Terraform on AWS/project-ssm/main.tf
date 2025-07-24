473d8a1a9cb89ab3ac31b56f3fcb20d94f9a3b1c
Kuljot Biring <ksbiring1@gmail.com>
2025-07-23 19:23:12 -0700

    add vpc endpoint resource

--- Terraform on AWS/project-ssm/main.tf
+++ Terraform on AWS/project-ssm/main.tf
@@ -21,3 +21,19 @@ module "networking" {
 module "ssm" {
   source = "./modules/ssm"
 }
+
+# Create VPC Endpoints
+resource "aws_vpc_endpoint" "vpc_endpoints" {
+  for_each = toset(var.vpc_endpoints) # Create a VPC endpoint for each service in the var.vpc_endpoints list
+  vpc_id = module.networking.vpc_resources.vpc_id
+  subnet_ids = module.networking.vpc_resources.private_subnet_ids
+  service_name = "com.amazonaws.${data.aws_region.current.name}.${each.key}"
+  vpc_endpoint_type = "Interface"
+  private_dns_enabled = true # Allow services within the VPC to automatically use the endpoint
+  security_group_ids = [aws_security_group.vpce_security_groups.id]
+
+  tags = {
+    "Name" = "tf_vpc_endpoint_${each.key}"
+  }
+}
+
