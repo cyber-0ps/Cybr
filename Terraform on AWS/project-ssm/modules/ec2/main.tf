3f411d97180da2f24cc2e1a9a5124f890bfe9f2a
Kuljot Biring <ksbiring1@gmail.com>
2025-07-24 18:42:02 -0700

    add aws instance

--- Terraform on AWS/project-ssm/modules/ec2/main.tf
+++ Terraform on AWS/project-ssm/modules/ec2/main.tf
@@ -18,3 +18,16 @@ data "aws_ami" "amazon_linux_latest" {
     values = ["x86_64"]
   }
 }
+
+resource "aws_instance" "instance" {
+  ami                         = data.aws_ami.amazon_linux_latest.id
+  instance_type               = var.ec2_config.instance_type
+  subnet_id                   = var.ec2_config.subnet_id
+  associate_public_ip_address = var.ec2_config.public_ip
+  vpc_security_group_ids      = var.ec2_config.security_groups
+  tags                        = var.ec2_config.tags
+
+  root_block_device {
+    encrypted = true
+  }
+}
