30c0067a418b4aed42eb5bb7a9d1c5cb2c6be417
Kuljot Biring <ksbiring1@gmail.com>
2025-07-27 10:45:51 -0700

    add encryption to bucket

--- Terraform on AWS/iac-scanning/terraform-compliance/main.tf
+++ Terraform on AWS/iac-scanning/terraform-compliance/main.tf
@@ -3,3 +3,12 @@ resource "aws_s3_bucket" "example" {
 
   tags = var.aws_tagging
 }
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.example.bucket
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
