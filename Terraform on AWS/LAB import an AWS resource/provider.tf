eddaa97ed85fa010dfed706a863508b9696fb105
Kuljot Biring <ksbiring1@gmail.com>
2025-09-14 23:43:38 -0700

    add provider file

--- Terraform on AWS/LAB import an AWS resource/provider.tf
+++ Terraform on AWS/LAB import an AWS resource/provider.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.3.0"
     }
   }
@@ -9,6 +9,6 @@ terraform {
 
 provider "aws" {
   # Configuration options
-  region = var.aws_region
+  region  = var.aws_region
   profile = "cybr"
 }
