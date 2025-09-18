4e9d1558a0f59760c0975fa766441b43b388fc39
Kuljot Biring <ksbiring1@gmail.com>
2025-09-17 23:02:26 -0700

    add outputs file

--- Terraform on AWS/project-ssm/modules/vpc/outputs.tf
+++ Terraform on AWS/project-ssm/modules/vpc/outputs.tf
@@ -1,9 +1,9 @@
 output "vpc_resources" {
   value = {
-    vpc_id = aws_vpc.id
-    public_subnet_ids = aws_subnet.public[*].id
-    private_subnet_ids = aws_subnet.private[*].id
+    vpc_id                  = aws_vpc.id
+    public_subnet_ids       = aws_subnet.public[*].id
+    private_subnet_ids      = aws_subnet.private[*].id
     aws_internet_gateway_id = aws_internet_gateway.igw.id
-    nat_gateway_ids = aws_nat_gateway.ngw[*].id
+    nat_gateway_ids         = aws_nat_gateway.ngw[*].id
   }
 }
