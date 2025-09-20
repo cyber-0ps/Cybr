b9494d0df785071b46da66f64a80ee54c8f19546
Kuljot Biring <ksbiring1@gmail.com>
2025-09-19 17:34:22 -0700

    add main file

--- Terraform on AWS/project-ssm/modules/vpc/main.tf
+++ Terraform on AWS/project-ssm/modules/vpc/main.tf
@@ -1,6 +1,6 @@
 # create local varibles to store the number of public and private subnets
 locals {
-  num_of_public_subnets = length(var.vpc_config.availability_zones)
+  num_of_public_subnets  = length(var.vpc_config.availability_zones)
   num_of_private_subnets = length(var.vpc_config.availability_zones)
 }
 
@@ -10,10 +10,10 @@ data "aws_region" "current" {}
 # VPC
 # create a VPC
 resource "aws_vpc" "vpc" {
-  cidr_block = var.vpc_config.cidr_block
+  cidr_block           = var.vpc_config.cidr_block
   enable_dns_hostnames = var.vpc_config.enable_dns_hostnames
-  enable_dns_support = var.vpc_config.enable_dns_support
-  tags = var.vpc_config.tags
+  enable_dns_support   = var.vpc_config.enable_dns_support
+  tags                 = var.vpc_config.tags
 }
 
 # create an Internet Gateway for our VPC
@@ -26,13 +26,13 @@ resource "aws_internet_gateway" "igw" {
 
 # Create public subnets
 resource "aws_subnet" "public" {
-  count = local.num_of_public_subnets
-  vpc_id = aws_vpc.vpc.id
+  count             = local.num_of_public_subnets
+  vpc_id            = aws_vpc.vpc.id
   availability_zone = var.vpc_config.availability_zones[count.index]
-  cidr_block = cidrsubnet(aws_vpc.vpc.cidr_block, 2, count.index +1) #function that automatically creates subnet CIDR blocks based n the VPC CIDR block
+  cidr_block        = cidrsubnet(aws_vpc.vpc.cidr_block, 2, count.index + 1) #function that automatically creates subnet CIDR blocks based n the VPC CIDR block
   tags = {
-    "Name" = "tf_public_subnet_${count.index +1}"
-  }  
+    "Name" = "tf_public_subnet_${count.index + 1}"
+  }
 }
 
 # Create route table for public subnets
@@ -46,36 +46,36 @@ resource "aws_route_table" "public" {
 
 # Assocaite the route table with the public subnets
 resource "aws_route_table_association" "public" {
-  count = local.num_of_public_subnets 
-  subnet_id = aws_subnet.public[count.index].id # gets each public subnet ID
+  count          = local.num_of_public_subnets
+  subnet_id      = aws_subnet.public[count.index].id # gets each public subnet ID
   route_table_id = aws_route_table.public.id
 }
 
 # Private subnets and associated route tables
 resource "aws_subnet" "private " {
-  count = local.num_of_private_subnets
-  vpc_id = aws_vpc.vpc.id
+  count             = local.num_of_private_subnets
+  vpc_id            = aws_vpc.vpc.id
   availability_zone = var.vpc_config.availability_zones[count.index]
-  cidr_block = cidrsubnet(aws_vpc.vpc.cidr_block, 4, count.index +1) #function that automatically creates subnet CIDR blocks based n the VPC CIDR block
+  cidr_block        = cidrsubnet(aws_vpc.vpc.cidr_block, 4, count.index + 1) #function that automatically creates subnet CIDR blocks based n the VPC CIDR block
   tags = {
-    "Name" = "tf_private_subnet_${count.index +1}"
-  }  
+    "Name" = "tf_private_subnet_${count.index + 1}"
+  }
 }
 
 # Create route tables for each private subnet
 resource "aws_route_table" "private" {
-    count = local.num_of_private_subnets
-    vpc_id = aws_vpc.vpc.id
+  count  = local.num_of_private_subnets
+  vpc_id = aws_vpc.vpc.id
   route = {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.ngw[count.index].id
   }
 }
 
 # Assocaite the route table with the private subnets
 resource "aws_route_table_association" "private" {
-  count = local.num_of_private_subnets 
-  subnet_id = aws_subnet.private[count.index].id # gets each private subnet ID
+  count          = local.num_of_private_subnets
+  subnet_id      = aws_subnet.private[count.index].id                  # gets each private subnet ID
   route_table_id = element(aws_route_table.private[*].id, count.index) # assocaite the privatesubnets with each private route tables
 }
 
@@ -90,10 +90,10 @@ resource "aws_eip" "ngw" {
 
 # Create a NAT Gateway for each public subnet
 resource "aws_nat_gateway" "ngw" {
-  count = local.num_of_public_subnets
-  allocation_id = element(aws_eip.ngw[*].id, count.index) # assign an elastic IP to each of the NAT Gateways
-  subnet_id = element(aws_subnet.public[*], count.index) # assign each NAT Gateway to a public subnet
-  depends_on = [ aws_internet_gateway.igw ] # Nat Gateway depends on Internet Gateway
+  count         = local.num_of_public_subnets
+  allocation_id = element(aws_eip.ngw[*].id, count.index)    # assign an elastic IP to each of the NAT Gateways
+  subnet_id     = element(aws_subnet.public[*], count.index) # assign each NAT Gateway to a public subnet
+  depends_on    = [aws_internet_gateway.igw]                 # Nat Gateway depends on Internet Gateway
 
   tags = {
     "Name" = "tf_nat_gateway_${count.index + 1}"
